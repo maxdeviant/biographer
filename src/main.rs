5fd39b6573bd52c99b9cf1437c94fa3c7f8dfca3
Marshall Bowers <elliott.codes@gmail.com>
2019-08-30 22:21:47 -0400

    Add a script for updating profiles

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,55 @@
-fn main() {
-    println!("Hello, world!");
+use dialoguer::Confirmation;
+
+struct Bio {
+    pub description: String,
+}
+
+struct Service {
+    pub name: String,
+    pub update_profile_url: String,
+}
+
+fn main() -> Result<(), &'static str> {
+    femme::start(log::LevelFilter::Trace).unwrap();
+
+    let bio = Bio {
+        description:
+            "宝霞龙. 25. Conjurer of code. Devourer of art. Pursuer of æsthetics. Soli Deo gloria."
+                .into(),
+    };
+
+    let services = vec![
+        Service {
+            name: "are.na".into(),
+            update_profile_url: "https://www.are.na/settings/general".into(),
+        },
+        Service {
+            name: "twitter".into(),
+            update_profile_url: "https://twitter.com/settings/profile".into(),
+        },
+        Service {
+            name: "merveilles.town".into(),
+            update_profile_url: "https://merveilles.town/settings/profile".into(),
+        },
+    ];
+
+    for service in services {
+        loop {
+            println!("{}", service.name);
+            println!("Open {}", service.update_profile_url);
+            println!("{}", bio.description);
+
+            let done = Confirmation::new()
+                .with_text(&format!("Are you finished updating {}?", service.name))
+                .interact()
+                .unwrap();
+            if done {
+                break;
+            }
+        }
+    }
+
+    println!("All your bios have been updated!");
+
+    Ok(())
 }
