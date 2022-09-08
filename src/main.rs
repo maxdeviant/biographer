1a13cd25758f81ae1024974662052a980924ac34
Marshall Bowers <elliott.codes@gmail.com>
2022-09-07 21:53:16 -0400

    Updates for 2022

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn main() -> Result<(), &'static str> {
 
     let bio = Bio {
         description:
-            "宝霞龙. 26. Conjurer of code. Devourer of art. Pursuer of æsthetics. Soli Deo gloria. He/him."
+            "宝霞龙. 28. Conjurer of code. Devourer of art. Pursuer of æsthetics. Soli Deo gloria. He/him."
                 .into(),
     };
 
